3d7c0b2b74981f853fb1e4005edfb58440ea1c9b
Poddar <nikhita.poddar@capgemini.com>
2021-11-25 12:47:50 +0530

    initial version

---
+++ src/main/java/com/exception/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.exception;
+import java.time.LocalDateTime;
+import java.util.Date;
+import com.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<?> customerNotFoundException(CustomerNotFoundException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(DuplicateCustomerException.class)
+    public ResponseEntity<?> duplicatecustomerException(DuplicateCustomerException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(DepartmentNotFoundException.class)
+    public ResponseEntity<?> departmentNotFoundException(DepartmentNotFoundException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(InvalidCredintialException.class)
+    public ResponseEntity<?> invalidcredintialException(InvalidCredintialException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(IssueNotFoundException.class)
+    public ResponseEntity<?> issuenotfoundException(IssueNotFoundException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(OperatorNotFoundException.class)
+    public ResponseEntity<?> operatornotfoundException(OperatorNotFoundException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    
+    
+	
+	  @ExceptionHandler(Exception.class) 
+	  public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) { ErrorDetails
+	  errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),request.getDescription(false)); 
+	  return new ResponseEntity<>(errorDetails,HttpStatus.INTERNAL_SERVER_ERROR); }
+	  
+	  
+	 
+		 
+}
